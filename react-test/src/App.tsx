cdda4aa0549de76499ed2e66d2d8c3243d1c0fe3
ElDiGio <samuel.dgv3@gmail.com>
2023-11-20 10:56:00 +0100

    added things

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Form, { UserData } from "./Form";
 
 type User = {
   id?: number;
@@ -13,24 +14,52 @@ const App = () => {
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const { data: users } = await axios.get<User[]>("http://localhost:3000/users");
-      setUsers(users);
+      try {
+        const { data: fetchedUsers } = await axios.get<User[]>("http://localhost:3000/users");
+        setUsers(fetchedUsers);
+      } catch (error) {
+        console.error(error);
+      }
     };
     fetchUsers();
   }, []);
 
+  const addUser = async (userData: UserData) => {
+    try {
+      await axios.post("http://localhost:3000/users", userData);
+      const updatedUsers = [...users, userData];
+      setUsers(updatedUsers);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteUser = async (userId: number) => {
+    try {
+      await axios.delete(`http://localhost:3000/users/${userId}`);
+      const updatedUsers = users.filter((user) => user.id !== userId);
+      setUsers(updatedUsers);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4">
-      <h1>Users List</h1>
-      {users.map((user, i) => {
-        return (
-          <ul key={i}>
+      <h1 className="text-4xl font-bold">Users List</h1>
+      {users.map((user, i) => (
+        <div className="flex items-center gap-x-4" key={i}>
+          <ul>
             <li>First Name: {user.firstName}</li>
             <li>Last Name: {user.lastName}</li>
             <li>Age: {user.age}</li>
           </ul>
-        );
-      })}
+          <button className="btn btn-outline btn-error" onClick={() => deleteUser(Number(user.id))}>
+            Delete
+          </button>
+        </div>
+      ))}
+      <Form addUser={addUser} />
     </div>
   );
 };
--- react-test/src/Form/index.tsx
+++ react-test/src/Form/index.tsx
@@ -1,22 +1,31 @@
-import { DateTime } from "luxon";
 import { ChangeEvent, useState } from "react";
 
-type UserData = {
+export type UserData = {
   firstName?: string;
   lastName?: string;
-  email?: string;
-  dateOfBirth?: string;
+  age?: number;
 };
 
-function Form() {
+function Form({ addUser }: { addUser: (userData: UserData) => void }) {
   const [userData, setUserData] = useState<UserData>();
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setUserData({
       ...userData,
-      [name]: name === "dateOfBirth" ? DateTime.fromISO(value).toLocaleString(DateTime.DATE_SHORT, { locale: "fr" }) : value,
+      [name]: value,
     });
+  };
+
+  const handleAddUser = () => {
+    if (userData?.firstName && userData?.lastName && userData?.age) {
+      addUser(userData);
+      setUserData({
+        firstName: "",
+        lastName: "",
+        age: 0,
+      });
+    }
   };
 
   return (
@@ -24,33 +33,33 @@ function Form() {
       <div className="flex flex-col gap-5 mx-5 my-5">
         <div className="card bg-neutral text-neutral-content">
           <div className="card-body">
-            <input type="text" name="firstName" placeholder="First Name" className="input input-bordered" onChange={handleInputChange} />
-            <input type="text" name="lastName" placeholder="Last Name" className="input input-bordered" onChange={handleInputChange} />
-            <input type="email" name="email" placeholder="Email" className="input input-bordered" onChange={handleInputChange} />
-            <input type="date" name="dateOfBirth" className="input input-bordered" onChange={handleInputChange} />
-          </div>
-        </div>
-
-        <div className="card bg-neutral text-neutral-content">
-          <div className="card-body">
-            <ul>
-              <li>
-                <strong>First Name: </strong>
-                <span>{userData?.firstName}</span>
-              </li>
-              <li>
-                <strong>Last Name: </strong>
-                <span>{userData?.lastName}</span>
-              </li>
-              <li>
-                <strong>Email: </strong>
-                <span>{userData?.email}</span>
-              </li>
-              <li>
-                <strong>Date of Birth: </strong>
-                <span>{userData?.dateOfBirth}</span>
-              </li>
-            </ul>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              className="input input-bordered"
+              onChange={handleInputChange}
+              value={userData?.firstName || ""}
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              className="input input-bordered"
+              onChange={handleInputChange}
+              value={userData?.lastName || ""}
+            />
+            <input
+              type="number"
+              name="age"
+              placeholder="Age"
+              className="input input-bordered"
+              onChange={handleInputChange}
+              value={userData?.age || ""}
+            />
+            <button className="btn btn-outline btn-primary" onClick={handleAddUser}>
+              Add User
+            </button>
           </div>
         </div>
       </div>
